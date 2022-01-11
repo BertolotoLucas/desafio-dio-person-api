ac98cd6313ed7c445f8ee7a124f1d2c2f7b99eaf
Lucas Bertoloto dos Santos <lucasbdoss@hotmail.com>
2022-01-11 18:53:16 -0300

    Created a super class to wherever class that use Mockito can extend

---
+++ src/test/java/br/com/lucasbertoloto/personapi/core/BaseUnitTest.java
@@ -0,0 +1,7 @@
+package br.com.lucasbertoloto.personapi.core;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public abstract class BaseUnitTest {}
